204bfe19c824c1d9dbd7d99ca9f6a747123a7c5b
seungjin <osjgiant1216@gmail.com>
2026-06-11 16:08:02 +0900

    feature/Category2

--- src/main/java/com/aivle/miniproject5_backend/domain/Category.java
+++ src/main/java/com/aivle/miniproject5_backend/domain/Category.java
@@ -7,8 +7,14 @@ import lombok.Getter;
 @Getter
 public enum Category {
     FICTION("소설"),
+    MYSTERY("추리/스릴러"),
+    FANTASY("판타지/SF"),
+    ROMANCE("로맨스"),
+    HISTORY("역사"),
+    SCIENCE("과학"),
     TECHNOLOGY("기술/IT"),
     SELF_HELP("자기계발"),
+    ECONOMY("경제/경영"),
     ETC("기타");
 
     private final String description;
